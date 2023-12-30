f0c9099a234774fe70df4fc50b979b1a471dbb31
SujonHasan <sujonhasan171@gmail.com>
2023-12-30 13:27:06 +0600

    todo form code added

--- src/features/todos/TodoList.js
+++ src/features/todos/TodoList.js
@@ -1,8 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrash, faUpload } from '@fortawesome/free-solid-svg-icons';
 
 function TodoList() {
+
+  const [newTodo, setNewTodo] = useState('');
+
+  const newItemSection = <form action="">
+    <label htmlFor="new-todo"> Enter a new todo item</label>
+    <div className='pr-5'>
+      <input type="text" id='new-todo' value={newTodo} onChange={(e) => setNewTodo(e.target.value)}
+        placeholder='Enter new todo'
+      />
+    </div>
+
+    <button className='submit'>
+      <FontAwesomeIcon icon={faUpload} />
+    </button>
+
+  </form>
+
+  let content; 
+
   return (
-    <div>TodoList</div>
+    <main>
+      <h1>Todo List</h1>
+      {newItemSection}
+    </main>
   )
 }
 
